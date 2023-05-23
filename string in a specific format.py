652a9037ec9cec98a798e0e59885325bb6a71d11
Sadik Al Jarif <78945097+jarif87@users.noreply.github.com>
2023-05-24 02:05:48 +0600

    Add files via upload

---
+++ string in a specific format.py
@@ -0,0 +1,9 @@
+# Twinkle, twinkle, little star,
+# 	How I wonder what you are! 
+# 		Up above the world so high,   		
+# 		Like a diamond in the sky. 
+# Twinkle, twinkle, little star, 
+# 	How I wonder what you are!
+
+
+print("Twinkle,twinkle,little star,\n\thow I wonder what you are!\n\t\tup above the worls so high\n\t\tlike a diamond in the sky\n twinkle,twinkle,little star\n\t  how i wonder what you are")
