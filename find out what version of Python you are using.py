c70d26c75deaa53378834526e701d18680308aa4
Sadik Al Jarif <78945097+jarif87@users.noreply.github.com>
2023-05-24 02:12:54 +0600

    Add files via upload

---
+++ find out what version of Python you are using.py
@@ -0,0 +1,18 @@
+
+import sys
+
+print("python version :",sys.version)
+
+print("=================")
+
+print(sys.version_info)
+
+
+print("==========Using platform module============")
+import platform
+
+print(platform.python_version())
+
+
+
+
