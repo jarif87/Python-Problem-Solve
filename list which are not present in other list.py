d2f294b0093126448361a5404e36a3dca393b74e
Sadik Al Jarif <78945097+jarif87@users.noreply.github.com>
2023-06-02 22:11:12 +0600

    Add files via upload

---
+++ list which are not present in other list.py
@@ -0,0 +1,17 @@
+list_0={"white","red","black","blue"}
+list_1={"crimson","red","white","orange","olive"}
+
+print("Orgnal List :",list_0)
+print("Orginal List :",list_1)
+
+print("==========================")
+
+print("=========New List After Transform=========")
+
+x=list_0.difference(list_1)
+
+print("New List :",x)
+
+y=list_1.difference(list_0)
+print("New List :",y)
+
