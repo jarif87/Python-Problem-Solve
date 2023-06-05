d0605e773d6240d6bc0519082f6505d6b0dda7e3
Sadik Al Jarif <78945097+jarif87@users.noreply.github.com>
2023-06-06 01:36:43 +0600

    Add files via upload

---
+++ two values are equal sum will be zero.py
@@ -0,0 +1,29 @@
+def sum_three(x,y,z):
+    if x==y or y==z or z==x:
+        sum=0
+    else:
+        sum=x+y+z
+    
+    return sum
+
+sum_three(10,20,30)
+
+#%%
+def sum_three(x,y,z):
+    if x==y or y==z or z==x:
+        sum=0
+    else:
+        sum=x+y+z
+    
+    return sum
+
+m=int(input("Entrer your number : "))
+
+n=int(input("Entrer your number : "))
+
+
+o=int(input("Entrer your number : "))
+
+sum_three(m,n,o)
+
+
