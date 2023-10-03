b66187361e4ebb0b29d0116b2235debbbda310db
Sadik Al Jarif <78945097+jarif87@users.noreply.github.com>
2023-10-03 20:49:18 +0600

    Add files via upload

---
+++ list problem slove.py
@@ -0,0 +1,20 @@
+list1 = [100, 200, 300, 400, 500]
+
+list1.reverse()
+print(list1)
+
+#%%
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
+list3=[i+j for i,j in zip(list1,list2)]
+print(list3)
+
+#%%
+final_result=[]
+numbers = [1, 2, 3, 4, 5, 6, 7]
+for x in numbers:
+    final_result.append(x*x)
+print(final_result)
+
+#%%
+
